42e8a00ccb8932fc56a5f192ed7ad61c2a6130aa
JoaoPedroFazzolo <143749763+JoaoPedroFazzolo@users.noreply.github.com>
2025-03-29 16:29:33 -0300

    fix: corrige o dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
-FROM openjdk:17-jdk
+FROM eclipse-temurin:17
+LABEL maintainer="admfazzolo@gmail.com"
 WORKDIR /app
-COPY target/itensPregao-0.0.1-SNAPSHOT.jar app.jar
-CMD ["java", "-jar", "app.jar"]
+COPY target/itensPregao-0.0.1-SNAPSHOT.jar /app/consultalicitacao.jar
+ENTRYPOINT ["java", "-jar", "consultalicitacao.jar"]
+EXPOSE 8080
--- src/main/java/br/com/joaopedrofazzolo/itenspregao/controller/CompraController.java
+++ src/main/java/br/com/joaopedrofazzolo/itenspregao/controller/CompraController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
