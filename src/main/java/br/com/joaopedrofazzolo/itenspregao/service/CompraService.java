fdc31ecba400a81b4579647145964ba2935652bb
JoaoPedroFazzolo <143749763+JoaoPedroFazzolo@users.noreply.github.com>
2025-04-24 17:22:48 -0300

    fix: atuliza a url de consulta da api do gov

--- src/main/java/br/com/joaopedrofazzolo/itenspregao/service/CompraService.java
+++ src/main/java/br/com/joaopedrofazzolo/itenspregao/service/CompraService.java
@@ -6,11 +6,11 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CompraService {
-    private static final String API_URL = "https://dadosabertos.compras.gov.br/modulo-contratacoes/2.1_consultarItensContratacoes_PNCP_14133_Id";
+    private static final String API_URL = "https://dadosabertos.compras.gov.br/modulo-contratacoes/2.1_consultarItensContratacoes_PNCP_14133_Id?tipo=idCompra&";
 
     public CompraResponseDTO obterDadosCompra(String idCompra) {
         RestTemplate restTemplate = new RestTemplate();
-        String url = API_URL + "?idCompra=" + idCompra;
+        String url = API_URL + "codigo=" + idCompra;
 
         CompraResponseDTO response = restTemplate.getForObject(url, CompraResponseDTO.class);
 
